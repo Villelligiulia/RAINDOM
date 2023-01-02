5a4add2fc6d98a80cf2ba76a029ab3d990452be1
Villelligiulia <villelligiulia@gmail.com>
2023-01-02 21:13:40 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,61 @@
+function emptyBoxGame(){}
+
+
+function changeColor(){}
+
+
+function question(){}
+
+function clearAnswer(){}
+
+function showAnswer(){}
+
+function levelTwo() {}
+
+function showAnswerLevelTwo(){}
+
+function levelThree() {}
+
+function showAnswerLevelThree(){}
+
+function levelFour() {}
+
+function showAnswerLevelFour(){
+}
+
+function increaseLevel() {}
+
+function levelFive() {}
+
+function showAnswerLevelFive(){}
+
+function levelSix() {}
+
+function showAnswerLevelSix(){}
+
+function levelSeven(){}
+
+function showAnswerLevelSeven(){}
+
+function levelEight() {}
+
+function showAnswerLevelEight(){}
+
+function levelNine() {}
+
+function showAnswerLevelNine(){}
+
+function levelTen() {}
+
+function showAnswerLevelTen(){}
+
+function startNewGame(){}
+
+function incrementScore(){}
+
+function incrementWrongAnswer(){}
+
+function decrementAttempts(){}
+
+
 
